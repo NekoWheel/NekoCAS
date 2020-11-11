cba289551b78c56fb8d237d2da5df28a85b06086
E99p1ant <i@github.red>
2020-11-11 23:18:58 +0800

    delete profile

--- web/router.go
+++ web/router.go
@@ -51,8 +51,6 @@ func Run() {
 		// CAS 协议实现
 		r.Get("/validate", middleware.ServicePreCheck, v1.ValidateHandler)        // v1
 		r.Get("/serviceValidate", middleware.ServicePreCheck, v2.ValidateHandler) // v2
-		//r.Get("/proxy", )
-		//r.Get("/proxyValidate", )
 	},
 		session.Sessioner(session.Options{
 			CookieName: "nekocas",
@@ -65,47 +63,5 @@ func Run() {
 
 		context.Contexter(),
 	)
-
-	//r.HTMLRender = middleware.LoadTemplates("./templates")
-	//store := sessions.NewCookieStore([]byte(config.Get().SessionKey))
-	//r.Use(sessions.Middleware("session", store))
-	//// CSRF Token
-	//r.Use(middleware.CSRF())
-	//
-	//// Account
-	//r.GET("/register", middleware.NotLoggedIn, middleware.OpenForRegister, account.RegisterViewHandler)
-	//r.POST("/register", middleware.NotLoggedIn, middleware.OpenForRegister, account.RegisterActionHandler)
-	//r.POST("/login", middleware.NotLoggedIn, middleware.ServicePreCheck, account.LoginActionHandler)
-	//r.GET("/", middleware.LoggedIn)
-	////r.GET("/profile", middleware.LoggedIn, cas.profileViewHandler)
-	////r.POST("/profile", middleware.LoggedIn, cas.profileActionHandler)
-	//
-	//// Service
-	//
-	//// CAS Protocol
-	//r.GET("/login", middleware.ServicePreCheck, cas.LoginViewHandler) // Login view
-	//r.POST("/logout", middleware.LoggedIn, cas.LogoutHandler)         // Logout action
-	//r.POST("/validate", middleware.LoggedIn, cas.ValidateHandler)     // Server-side validate action
-	//
-	//// service first time login, ask user for permission.
-	////r.POST("/authorize", cas.authRequired, cas.authorizeHandler)
-	////r.POST("/revoke", cas.authRequired, cas.revoke)
-	////
-	////r.GET("/validate", cas.validateHandler)
-	//
-	//// manager
-	////manage := r.Group("/manage", cas.authRequired, cas.adminRequired)
-	////manage.GET("/", cas.managerViewHandler)
-	////manage.POST("/service/ban", cas.switchBanServiceHandler)
-	////manage.POST("/service/delete", cas.deleteServiceHandler)
-	////manage.GET("/service/new", cas.newServiceViewHandler)
-	////manage.POST("/service/new", cas.newServiceHandler)
-	////
-	////manage.POST("/user/admin", cas.setAdminHandler)
-	////manage.POST("/user/delete", cas.deleteAdminHandler)
-	//
-	//err := r.Run(":" + strconv.Itoa(config.Get().Port))
-	//log.Fatal("Failed to start web server: %v", err)
-
 	r.Run()
 }
